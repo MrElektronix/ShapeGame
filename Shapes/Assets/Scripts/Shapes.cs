c2eaa07489603213e5b52caae4d428d27c385b13
MrElektronix <milovanpelt@gmail.com>
2017-12-27 00:09:21 +0100

    Removed & Changed scripts
    Removed:
    - BlockShape Script
    - TriangleShape  Script
    - CircleShape Script
    - Game Script
    
    Changed:
    - Shapes Script {Shapes are now added in the inspector in the shapesList}

---
+++ Shapes/Assets/Scripts/Shapes.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shapes : MonoBehaviour {
+
+    public List<GameObject> shapesList = new List<GameObject>();
+
+    private float size;
+    private string direction;
+
+    private float speedX;
+    private float speedY;
+
+    private Vector3 moveVector;
+    
+    private void Start()
+    {
+        size = 4f;
+        speedX = 0.05f;
+        speedY = 0.05f;
+        moveVector = new Vector3(speedX, speedY, 0);
+
+        CreateShapes();
+    }
+
+    private void Update()
+    {
+        MoveShapes();
+    }
+
+    private void CreateShapes()
+    {
+        if (shapesList != null)
+        {
+            for (int i = 0; i < shapesList.Count; i++)
+            {
+                shapesList[i] = Instantiate(shapesList[i], new Vector3(0f, 0f, 0f), Quaternion.identity);
+            }
+        }
+    }
+
+    private void MoveShapes()
+    {
+        if (shapesList != null)
+        {
+            for (int i = 0; i < shapesList.Count; i++)
+            {
+                shapesList[i].transform.position += moveVector;
+            }
+        }
+    }
+}
